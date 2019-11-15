ebffcab53b2475cc3a6997223e149cbfe7a78120
Jacob Prudent <jake.prudent@gmail.com>
2019-11-15 10:59:32 -0800

    almost there

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,22 +1,23 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
 
-class CuboidMaker{
-    construtor(length, width, height){
-    this.length=length;
-    this.width= width;
-    this.height=height;
+class CuboidMaker2{
+    construtor(info){
+    this.length = info.length,
+    this.width = info.width,
+    this.height = info.height
     }
     volume(){
-        return this.length*this.width*this.height;
+        return (this.length * this.width * this.height);
       }
     surfaceArea(){
-        return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+        console.log("I am running")
+        return (2 * (this.length * this.width + this.length * this.height + this.width * this.height));
       }
-  };
+  }
 
-const cuboid= new CuboidMaker(4, 5, 5);
+const cuboid2= new CuboidMaker2({length:5, width:5, height:5});
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-console.log(cuboid.volume()); // 100
-console.log(cuboid.surfaceArea()); // 130
+console.log(cuboid2.volume()); // 100
+console.log(cuboid2.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -76,23 +76,33 @@ This will be an array of strings.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
-function pullio(graduate){
- return:;
-}
-universities.push(graduates.forEach(function(){return this.university}));
+
+graduates.forEach(function(value){universities.push(value.university)});
+universities.sort();
+
 console.log(universities);
 
-/* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
+/* Request 2: Create a new array called contactInfo that contains both first name and email
+ of each student. This will be an array of strings.
 
-The resulting contact information strings should have a space between the first name and the email, like this: 
+The resulting contact information strings should have a space between the first name
+ and the email, like this: 
 "Josh josh@example.com"
 
 Log the result of your new array. */
 const contactInfo = [];
+graduates.forEach(function(value){contactInfo.push(`${value.first_name} ${value.email}`)})
 console.log(contactInfo);
 
-/* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
+/* Request 3: Find out how many universities have the string "Uni" included in their name.
+ Create a new array called unisWithUni that contains them all. This will be an array of objects.
+  Log the result. */
 const unisWithUni = [];
+graduates.forEach(function(value){
+  if(value.university.search("Uni")>-1){
+    unisWithUni.push(value)
+  }
+})
 console.log(unisWithUni);
 
 
@@ -115,35 +125,46 @@ const zooAnimals = [
 
 /* Request 1: .forEach()
 
-The zoos want to display both the scientific name and the animal name in front of the habitats. Populate the displayNames array with only the animal_name and scientific_name of each animal. displayNames will be an array of strings, and each string should follow this pattern: "Name: Jackal, asiatic, Scientific: Canis aureus."
+The zoos want to display both the scientific name and the animal name in front of the habitats.
+ Populate the displayNames array with only the animal_name and scientific_name of each animal.
+  displayNames will be an array of strings, and each string should follow this pattern:
+   "Name: Jackal, asiatic, Scientific: Canis aureus."
 
 */
 const displayNames = [];
+zooAnimals.forEach(function(value){displayNames.push(`Name:${value.animal_name}, Scientific: ${value.scientific_name}`)})
 console.log(displayNames);
 
 /* Request 2: .map()
 
-The zoos need a list of all their animal's names (animal_name only) converted to lower case. Using map, create a new array of strings named lowCaseAnimalNames, each string following this pattern: "jackal, asiatic". Log the resut.
+The zoos need a list of all their animal's names (animal_name only) converted to lower case. 
+Using map, create a new array of strings named lowCaseAnimalNames,
+ each string following this pattern: "jackal, asiatic". Log the resut.
 
 */
 
-const lowCaseAnimalNames = [];
+const lowCaseAnimalNames = zooAnimals.map(function(value){return value.animal_name.toLowerCase()});
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
 
-The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
+The zoos are concerned about animals with a lower population count.
+ Using filter, create a new array of objects called lowPopulationAnimals
+  which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals = [];
+const lowPopulationAnimals = zooAnimals.filter(value => value.population<5);
 console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
 
-The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
+The zoos need to know their total animal population across the United States.
+ Find the total population from all the zoos using the .reduce() method.
+  Remember the reduce method takes two arguments:
+   a callback (which itself takes two args), and an initial value for the count.
 
 */
-const populationTotal = 0;
+const populationTotal = zooAnimals.reduce(function(accumulator, currentValue){return accumulator+currentValue.population}, 0);
 console.log(populationTotal);
 
 
